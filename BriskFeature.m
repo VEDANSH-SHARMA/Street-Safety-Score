0911b39191ec17c9ae995601001f4658ee67ddb9
Divyanshi123 <15uec023@lnmiit.ac.in>
2017-06-20 12:02:36 +0530

    Add files via upload

--- BriskFeature.m
+++ BriskFeature.m
@@ -2,7 +2,7 @@ clc;
 clear all;
 close all;
 
-for i= 1:5
+for i= 1
    str1='C:\Users\DSP_03\Desktop\Pics_1\';
    num=num2str(i);
    str2='.jpg';
@@ -13,6 +13,8 @@ for i= 1:5
    I = rgb2gray(a);
    
   points = detectBRISKFeatures(I);
+  [features, valid_points] = extractFeatures(I, points);
+  b=reshape(features.Features,[1,27520]);
   figure(i);
   imshow(I); hold on;
   plot(points.selectStrongest(1000));
