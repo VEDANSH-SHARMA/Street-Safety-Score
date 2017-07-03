994a72d2a298e5367636b1e9e5b2c4923ded8dbe
Yash Jain <yashjain.lnm@gmail.com>
2017-07-03 18:51:27 +0530

    garbage_removed

--- features.m
+++
@@ -1,8 +0,0 @@
-img=imread('C:\Users\DSP_19\Desktop\StreetScore\Pics_1\1 - Copy.jpg');
-figure;
-imshow(img);
-[featureVector, hogVisualization] = extractHOGFeatures(img);
-figure;
-imshow(img); 
-hold on;
-plot(hogVisualization);
